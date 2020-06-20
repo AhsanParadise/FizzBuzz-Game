79ac598ca48391f87f7e1630399cdb52b7657d3c
AhsanParadise <63900641+AhsanParadise@users.noreply.github.com>
2020-06-20 03:48:48 -0700

    add

---
+++ Fizz_Buzz.py
@@ -0,0 +1,25 @@
+
+# ----------- F i z z B u z z  G a m e ----------#
+
+
+FizzBuzz = input('Type a number: ')
+
+if (float(FizzBuzz) % 3 == 0) and (float(FizzBuzz) % 5 == 0):
+    print('F i z z B u z z')
+elif float(FizzBuzz) % 3 == 0:
+    print('F i z z')
+elif float(FizzBuzz) % 5 == 0:
+    print('B u z z')
+else:
+    print(''' 
+ z
+  z
+   z
+    z
+     z
+      z
+       z   
+Sorry invalid number. Try 5, 6, 15, 20, 25, -15..........
+Thanks for playing this hilarious game. This game is very important for you.
+
+    ''')
