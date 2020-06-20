306cdb97d13fdcc557084d92c5874bb3f94212c0
AhsanParadise <63900641+AhsanParadise@users.noreply.github.com>
2020-06-20 03:50:26 -0700

    Update

--- Fizz_Buzz.py
+++ Fizz_Buzz.py
@@ -13,12 +13,12 @@ elif float(FizzBuzz) % 5 == 0:
 else:
     print(''' 
  z
-  z
-   z
-    z
-     z
-      z
-       z   
+  zz
+   zzz
+    zzzz
+     zzzzz
+      zzzzzz
+       zzzzzzz
 Sorry invalid number. Try 5, 6, 15, 20, 25, -15..........
 Thanks for playing this hilarious game. This game is very important for you.
 
